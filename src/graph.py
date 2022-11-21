7facc59bccf7aef6ea459eb836702b3e783fe611
vmleroy <victorlrmatos@gmail.com>
2022-11-20 22:01:42 -0300

    fitness, mutation, population created. also added geek for geeks code as a example to follow

--- src/graph.py
+++ src/graph.py
@@ -1,5 +1,6 @@
 class Vertice:
-    def __init__(self, nome):
+    def __init__(self, valor, nome: str = None):
+        self.valor = valor
         self.nome = nome
         self.color = None
         self.fitness = None
@@ -18,6 +19,7 @@ class Vertice:
     def __str__(self):
         return (
             "Nome: " + str(self.nome) + "     " +
+            "Valor: " + str(self.valor) + "     " +
             "Color: " + str(self.color) + "     " +
             "Fitness: " + str(self.fitness) + "     " +
             "Mapped Fitness: " + str(self.mappedFitness)
@@ -28,30 +30,30 @@ class Grafo:
         self.vertices = {}
         self.directed = directed
 
-    def adiciona_vertice(self, nome_vertice) -> Vertice:
+    def adiciona_vertice(self, valor, nome_vertice: str = None) -> Vertice:
         # importante pois podem haver vertices que não tem arestas
-        novo_vertice = Vertice(nome_vertice)
-        self.vertices[nome_vertice] = novo_vertice
+        novo_vertice = Vertice(valor, nome_vertice)
+        self.vertices[valor] = novo_vertice
         return novo_vertice
 
-    def adiciona_aresta(self, nome_origem, nome_destino):
-        vertice_origem = self.obtem_vertice(nome_origem)
-        vertice_destino = self.obtem_vertice(nome_destino)
+    def adiciona_aresta(self, valor_origem, valor_destino):
+        vertice_origem = self.obtem_vertice(valor_origem)
+        vertice_destino = self.obtem_vertice(valor_destino)
         if not vertice_origem is None and not vertice_destino is None:
             vertice_origem.insere_adjacencias(vertice_destino)
             if not self.directed:
                 vertice_destino.insere_adjacencias(vertice_origem)
 
-    def obtem_vertice(self, nome_vertice) -> Vertice:
-        if nome_vertice in self.vertices:
-            return self.vertices[nome_vertice]
+    def obtem_vertice(self, valor_vertice) -> Vertice:
+        if valor_vertice in self.vertices:
+            return self.vertices[valor_vertice]
         else:
             return None
 
     def print_graph(self):
-        for nome_vertice, vertice in self.vertices.items():
+        for valor_vertice, vertice in self.vertices.items():
             print(f"vertice: \n    {vertice}")
             print("vizinhos: ")
-            for nome_adj, adj in vertice.adjacencias.items():
+            for valor_adj, adj in vertice.adjacencias.items():
                 print(f"    {adj}")
             print("")
